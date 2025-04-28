bdfdb082a1e0030261a02c1e2b3276038f2239ce
CarlosPerez1999 <carlos.al.perez.hdez99@gmail.com>
2025-04-28 17:27:09 -0600

    (feat): add task service

---
+++ src/tasks/services/task.service.ts
@@ -0,0 +1,34 @@
+import { PrismaClient, type Task } from "../../generated/prisma";
+
+class TaskService {
+  private prisma = new PrismaClient();
+
+  async getTasks(): Promise<Task[]> {
+    return this.prisma.task.findMany();
+  }
+
+  async getTaskById(id: number): Promise<Task | null> {
+    return this.prisma.task.findUnique({
+      where: { id },
+    });
+  }
+
+  async createTask(data: Task): Promise<Task | null> {
+    return this.prisma.task.create({data});
+  }
+
+  async deleteTask(id: number): Promise<Task | null> {
+    return this.prisma.task.delete({
+      where: { id },
+    });
+  }
+
+  async updateTask(id: number, data: Task): Promise<Task | null> {
+    return this.prisma.task.update({
+      where: { id },
+      data: data,
+    });
+  } 
+}
+
+export const taskService = new TaskService()
