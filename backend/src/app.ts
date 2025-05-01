86ce23c8a6b754b4ee6bcbc4693f66348c75da56
CarlosPerez1999 <carlos.al.perez.hdez99@gmail.com>
2025-04-30 21:15:23 -0600

    (feat): add CORS policies to the server

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,16 +1,21 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import taskRoutes from './tasks/routes/task.routes'
+import cors from 'cors'
 
 dotenv.config()
 
 const app = express()
 const port = process.env.PORT
 
-app.use(express.json())
+const corsOptions: cors.CorsOptions = {
+  origin: ['http://localhost:4200',], 
+};
 
-app.use('/tasks', taskRoutes)
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use('/tasks', taskRoutes);
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`)
-})
+});
