886322711eed45a2b677450bf5dd50ddac5b148a
CarlosPerez1999 <carlos.al.perez.hdez99@gmail.com>
2025-05-01 12:53:04 -0600

    (feat): add frontend initial routes and implement the main router outlet

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { TodosPageComponent } from './todos/pages/todos-page/todos-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:"",
+    component: TodosPageComponent
+  },
+  {
+    path:"**",
+    redirectTo:""
+  }
+];
